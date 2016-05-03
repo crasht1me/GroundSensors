d1f7370a6444302cc3d8ab8f886e76808469cecd
s156676 <s156676@S156676>
2016-05-03 15:17:30 +0200

    Add project files.

---
+++ GroundSensors/GroundSensors.ino
@@ -0,0 +1,21 @@
+/*
+ Name:		GroundSensors.ino
+ Created:	5/3/2016 3:04:15 PM
+ Author:	s156676
+*/
+
+// the setup function runs once when you press reset or power the board
+
+int Ground_Sensor_Left_pin = A0;
+int Ground_Sensor_Right_pin = A2;
+
+
+void setup() 
+{
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
